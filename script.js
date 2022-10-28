2a6ff5527da83cc9cb82899728e96ffd63cc0038
Thoufeek <thoufeekrahuman9361@gmail.com>
2022-10-28 16:03:08 +0530

    10/28

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
-
-const productsHeader = ['Art No', 'size', 'color', 'MRP'];
-const formEl = document.querySelector('form');
-const addProductFormEL = document.querySelector('.modal-content form');
+const productsHeader = ["Art No", "size", "color", "MRP"];
+const formEl = document.querySelector("form");
+const addProductFormEL = document.querySelector(".modal-content form");
+let isSecondClick = false;
+let divRef = null;
+const checkedValues = [];
 console.log(addProductFormEL);
 
+const refresh = () => {};
 
-formEl.addEventListener('submit', (e) => {
-    e.preventDefault(); 
-    const list = JSON.parse(localStorage.getItem(formEl.productsearch.value));
-    const div = document.createElement('div');
-    const divChild = document.createElement('div');
-    const ul = document.createElement('ul');
-    for(i = 0; i <= 3; i++) {
-      const li = document.createElement('li');
-      li.textContent = productsHeader[i];
-      ul.append(li);
-    }
-    divChild.append(ul);
-    const ulMain = document.createElement('ul');
-    
-    list.forEach(spec => {
-     const liMain = document.createElement('li');
-      liMain.textContent = spec;
-      ulMain.append(liMain);
-    });
-    div.append(divChild)
-    div.append(ulMain);
-    document.body.append(div);
-})
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const div = document.createElement("div");
+  if (isSecondClick) {
+    divRef.remove();
+    divRef = div;
+  } else {
+    divRef = div;
+    isSecondClick = true;
+  }
 
-const addIcon = document.querySelector('.fa-circle-plus');
-const removeIcon = document.querySelector('.fa-trash');
-const containerEl = document.querySelector('.container')
+  const list = JSON.parse(localStorage.getItem(formEl.productsearch.value));
 
-const divEl = document.createElement('div');
-const ulEl = document.createElement('ul');
+  div.classList.add("products-list");
+  // const divChild = document.createElement("div");
+  const ul = document.createElement("ul");
+  for (i = 0; i <= 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = productsHeader[i];
+    ul.append(li);
+    // const value = document.createElement('li');
+    // value.textContent = list[i];
+    // ul.append(value)
+  }
+  div.append(ul);
+  // const ulMain = document.createElement("ul");
 
+  list.forEach((spec) => {
+    const li = document.createElement("li");
+    li.textContent = spec;
+    ul.append(li);
+  });
+  // // div.append(divChild);
+  // div.append(ulMain);
+  document.body.append(div);
+});
 
+const addIcon = document.querySelector(".fa-circle-plus");
+const removeIcon = document.querySelector(".fa-trash");
+const containerEl = document.querySelector(".container");
 
-
+const divEl = document.createElement("div");
+const ulEl = document.createElement("ul");
 
 //copy code
 let modal = document.querySelector(".modal");
@@ -48,7 +60,7 @@ function toggleModal() {
   modal.classList.toggle("show-modal");
 }
 function windowOnClick(event) {
-  if(event.target === modal) {
+  if (event.target === modal) {
     toggleModal();
   }
 }
@@ -56,20 +68,32 @@ function windowOnClick(event) {
 function handleAddProducts(event) {
   event.preventDefault();
   console.log(addProductFormEL.elements);
-  console.log(addProductFormEL.elements[0].value);
+  // console.log(addProductFormEL.elements[0].value);
+  console.log(addProductFormEL.elements.sizes);
 
-  
+  const sizesList = [...addProductFormEL.elements.sizes];
+  console.log(sizesList);
 
-  localStorage.setItem( addProductFormEL.elements[0].value, 
-    JSON.stringify([addProductFormEL.elements[0].value,
-      addProductFormEL.elements[1].value, addProductFormEL.elements[2].value, addProductFormEL.elements[3].value])
+  sizesList.forEach((size) => {
+    if (size.checked === true) {
+      checkedValues.push(size.value);
+    }
+  });
+
+  console.log(checkedValues);
+
+  localStorage.setItem(
+    addProductFormEL.elements[0].value,
+    JSON.stringify([
+      addProductFormEL.elements[0].value,
+      checkedValues,
+      addProductFormEL.elements[2].value,
+      addProductFormEL.elements[3].value,
+    ])
   );
-
 }
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-addProductFormEL.addEventListener('submit', handleAddProducts);
-
-
+addProductFormEL.addEventListener("submit", handleAddProducts);
