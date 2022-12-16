53002f8a437b48930bd7a3c965e511e7d04f7552
Thoufeek <thoufeekrahuman9361@gmail.com>
2022-12-16 21:16:18 +0530

    12/15

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
+// Headers for products
 const productsHeader = ["Art No", "size", "color", "MRP"];
+//Getting the Main Form
 const formEl = document.querySelector("form");
+
+// Getting Modal content form
 const addProductFormEL = document.querySelector(".modal-content form");
+//To remove the available product list when the user checks for the second input
 let isSecondClick = false;
 let divRef = null;
+//To get the checked values of the checkbox
 const checkedValues = [];
 let elements = [];
+//To add availabe pmroducts to this div
 const container = document.getElementsByClassName(".products");
 console.log(addProductFormEL);
 
 //This is the closures method of passing arguments to callback function
 const handleCart = (list, items) => {
+  console.log("sold button is clicked");
   var data = list;
   var items = items;
-  const sizeSet = [];
-  const myStorage = JSON.parse(localStorage.getItem(list.artNo));
+  const addToCart = [];
+  const myStorage = JSON.parse(localStorage.getItem("products"));
+  console.log(myStorage);
   return () => {
     // console.log(data);
     for (i = 0; i < items.length; i++) {
       if (items[i].checked) {
-        sizeSet.push(items[i].value);
-        myStorage.sizes.splice(myStorage.sizes.indexOf(items[i].value), 1);
-        console.log(myStorage.sizes);
+        // addToCart.push(items[i].value);
+        // myStorage.sizes.splice(myStorage.sizes.indexOf(items[i].value), 1);
+        // console.log(myStorage.sizes);
       }
 
       localStorage.setItem(
@@ -35,7 +44,7 @@ const handleCart = (list, items) => {
     }
     const products = {
       artNo: list.artNo,
-      sizes: sizeSet,
+      sizes: addToCart,
       color: list.color,
       MRP: list.MRP,
     };
@@ -52,7 +61,9 @@ const handleCart = (list, items) => {
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
+  //adding available products to the div
   const div = document.createElement("div");
+  //To display the sold checkbox
   const soldContainer = document.createElement("div");
   if (isSecondClick) {
     divRef.remove();
@@ -65,42 +76,58 @@ formEl.addEventListener("submit", (e) => {
     isSecondClick = true;
   }
 
-  const list = JSON.parse([localStorage.getItem(formEl.productsearch.value)]);
+  const list = JSON.parse([localStorage.getItem("products")]);
   console.log(list);
+  // const availabeListArr = [...list];
+  // console.log(availabeListArr);
+  console.log(list[0].artNo);
+  console.log(formEl.productsearch.value);
 
-  div.classList.add("products-list");
-  const ul = document.createElement("ul");
-  for (i = 0; i <= 3; i++) {
-    const li = document.createElement("li");
-    li.textContent = productsHeader[i];
-    ul.append(li);
-  }
-  div.append(ul);
+  for (let i = 0; i < list.length; i++) {
+    console.log("Inside list for loop");
+    if (list[i].artNo === formEl.productsearch.value) {
+      console.log("Inside list if");
 
-  for (i = 0; i <= 3; i++) {
-    const li = document.createElement("li");
-    li.textContent = Object.values(list)[i];
-    ul.append(li);
-  }
-  document.body.append(div);
+      const listArr = Object.values(list[i]);
+      console.log(listArr[1].length);
+      div.classList.add("products-list");
+      const ul = document.createElement("ul");
+      for (i = 0; i <= 3; i++) {
+        const li = document.createElement("li");
+        li.textContent = productsHeader[i];
+        ul.append(li);
+      }
+      div.append(ul);
 
-  let checkBox = null;
-  let checkBoxList = [];
-  for (i = 0; i < list.sizes.length; i++) {
-    const label = document.createElement("label");
-    label.textContent = list.sizes[i];
-    checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
-    checkBox.value = list.sizes[i];
-    soldContainer.append(label, checkBox);
-    checkBoxList.push(checkBox);
+      for (j = 0; j <= 3; j++) {
+        const li = document.createElement("li");
+        // li.textContent = Object.values(list[i])[i];
+        li.textContent = listArr[j];
+        // li.textContent = list[i][j]
+        ul.append(li);
+      }
+      document.body.append(div);
+
+      let checkBox = null;
+      let checkBoxList = [];
+      for (j = 0; j < listArr[1].length; j++) {
+        console.log("Inside Checkbox");
+        const label = document.createElement("label");
+        label.textContent = listArr[1][j];
+        checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.value = listArr[1][j];
+        soldContainer.append(label, checkBox);
+        checkBoxList.push(checkBox);
+      }
+      const soldButton = document.createElement("button");
+      soldButton.textContent = "Sold";
+      soldContainer.append(soldButton);
+      document.body.append(soldContainer);
+      // const buyItems = [...checkBox.checked.value];
+      soldButton.addEventListener("click", handleCart(listArr, checkBoxList));
+    }
   }
-  const soldButton = document.createElement("button");
-  soldButton.textContent = "Sold";
-  soldContainer.append(soldButton);
-  document.body.append(soldContainer);
-  // const buyItems = [...checkBox.checked.value];
-  soldButton.addEventListener("click", handleCart(list, checkBoxList));
 });
 
 const addIcon = document.querySelector(".fa-circle-plus");
@@ -113,14 +140,13 @@ const existingItem = document.getElementById("existing-item");
 //Code for Existing Item
 
 existingItem.addEventListener("click", () => {
-
   toggleModal();
-  const select = document.createElement('select');
-  for(i = 0; i < addProductFormEL.children.length; i++) {
+  const select = document.createElement("select");
+  for (i = 0; i < addProductFormEL.children.length; i++) {
     elements[i] = addProductFormEL.children[i];
-    const option = document.createElement('option');
-    option.value = localStorage.getItem(addProductFormEL.artNo)
-    addProductFormEL.replaceChild()
+    const option = document.createElement("option");
+    option.value = localStorage.getItem(addProductFormEL.artNo);
+    addProductFormEL.replaceWith(select);
   }
 });
 
@@ -163,12 +189,13 @@ function handleAddProducts(event) {
   console.log(addProductFormEL.elements.sizes);
 
   const sizesList = [...addProductFormEL.elements.sizes];
-  console.log(sizesList);
+  console.log("size list", sizesList);
 
   sizesList.forEach((size) => {
     if (size.checked === true) {
       checkedValues.push(size.value);
     }
+    console.log("checked values", checkedValues);
   });
 
   const productDetails = {
@@ -180,7 +207,16 @@ function handleAddProducts(event) {
 
   console.log(...productDetails.color);
 
-  localStorage.setItem(productDetails.artNo, JSON.stringify(productDetails));
+  // Adding New Product to lacal storage
+  // localStorage.setItem(, JSON.stringify(productDetails));
+
+  localStorage.setItem(
+    "products",
+    JSON.stringify([
+      ...JSON.parse(localStorage.getItem("products") || "[]"),
+      productDetails,
+    ])
+  );
 }
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
